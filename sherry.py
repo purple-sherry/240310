ff1f4de442060e0ec359c2445f917d50ff868dca
purple-sherry <155607273+purple-sherry@users.noreply.github.com>
2024-03-13 00:14:31 +0900

    Update sherry.py

--- sherry.py
+++ sherry.py
@@ -26,7 +26,7 @@ ma_date = 5
 cut_rate_overraise = 1.10  #전일 상승폭이 너무 높은 코인 제외
 
 buy_rate = [0.34, 0.33, 0.33]
-emergency_sell_rate = [1.055, 1.053, 1.051]
+emergency_sell_rate = [1.05, 1.05, 1.05]
 
 def send_message(msg):
     """디스코드 메세지 전송"""
